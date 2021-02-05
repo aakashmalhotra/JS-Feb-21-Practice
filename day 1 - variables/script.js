26b08f9c188b0cb3b5d162ea3f5e51e85b9e3474
Your Name <you@example.com>
2021-02-05 15:32:51 -0500

    day 1

---
+++ day 1 - variables/script.js
@@ -0,0 +1,37 @@
+// External JS: The way to go
+
+console.log("Heeyyyy!!! My first program in JS");
+
+
+let foo = 5;
+let bar = 10;
+
+let foobar = foo + bar;
+
+console.log(foobar);
+
+
+// If condition
+if (foobar == 15) {
+    console.log("I am inside if condition");
+}
+
+// If-else condition
+if (foobar > 15) {
+    console.log("In if part of if-else");
+}
+else {
+    console.log("In else part of if-else");
+}
+
+
+// If else-if
+if (foobar < 10) {
+    console.log("I am inside if condition of if-else-if");
+}
+else if (foobar == 10) {
+    console.log("I am inside else-if condition of if-else-if");
+}
+else {
+    console.log("I am inside else condition of if-else-if");
+}
