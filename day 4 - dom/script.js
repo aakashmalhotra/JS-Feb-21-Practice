b141725100276798c55684926c84fab96b5284c0
Your Name <you@example.com>
2021-02-10 13:43:01 -0500

    day-4

---
+++ day 4 - dom/script.js
@@ -0,0 +1,9 @@
+function changeTheHeaderColor() {
+    let headingEl = document.getElementById("heading");
+    headingEl.style = "color: red; font-size:60px;";
+    headingEl.style = "padding: 20px;" // overides the properties from before
+}
+
+// function changeTheHeaderColor() {
+//     document.getElementById("heading").style = "color: red";
+// }
