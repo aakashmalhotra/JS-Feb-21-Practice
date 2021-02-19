ff096763683360810546d20b6f8b096a3939a867
Your Name <you@example.com>
2021-02-19 11:10:47 -0500

    day-11

--- template/script.js
+++ template/script.js
@@ -1,27 +1,2 @@
-function getPokemon() {
 
-    // fetch value from the text box
-    const textboxEl = document.getElementById("poke");
-    const pokemonName = textboxEl.value;
 
-    //  Make Ajax call using the fetched value
-    const url = "https://pokeapi.co/api/v2/pokemon/" + pokemonName;
-
-    httpRequest = new XMLHttpRequest();
-    httpRequest.open('GET', url);
-    httpRequest.onload = respHandler;
-    httpRequest.send();
-
-}
-
-function respHandler(resp) {
-    // Display the data received
-    const respJson = resp.target.response;
-
-    const respObject = JSON.parse(respJson);
-
-    const containerDivEl = document.getElementById("container");
-    const newDivEl = document.createElement("p");
-    newDivEl.innerText = respObject.name;
-    containerDivEl.appendChild(newDivEl);
-}
