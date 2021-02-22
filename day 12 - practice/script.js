573ad6afd283697372e87df64664bd7478f0a0f2
Your Name <you@example.com>
2021-02-22 10:59:14 -0500

    day-12

--- day 12 - practice/script.js
+++ day 12 - practice/script.js
@@ -1,7 +1,8 @@
 // Create a website that tells how long it would take to double your
 // money given a certain percentage of return per year.
 
-function calcTime() {
+function calcTime(event) {
+    console.log(event);
     const returnOnInvEl = document.getElementById("returnOnInv");
     const returnOnInv = returnOnInvEl.value;
 
@@ -9,6 +10,18 @@ function calcTime() {
     //  72/number in the text box
 
     const cotainerEl = document.getElementById("container");
-    cotainerEl.innerText = `Your money would double in ${time} years`
+    cotainerEl.innerText = `Your money would double in ${time} years.`
+    cotainerEl.innerText = "Your money would double in " + time + " years.";
 }
 
+function greet() {
+    console.log("HO HO HO")
+}
+
+function checkEnterPressed(event) {
+    console.log("key was pressed" + event.which);
+    if (event.which === 13) {
+        calcTime();
+        greet();
+    }
+}
